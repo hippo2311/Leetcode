43a47106c252a29f9692b5ce6fc15ae759416c23
hippo2311 <tobaochau05@gmail.com>
2024-12-04 22:03:48 +0700

    new file:   Arrays & String/1768. Merge Strings Alternately.py

---
+++ Arrays & String/1768. Merge Strings Alternately.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        a, b = 0, 0
+        r = []
+        for i in range(max(len(word1), len(word2))):
+            if a < len(word1):
+                r.append(word1[a])
+                a += 1
+            if b < len(word2):
+                r.append(word2[b])
+                b += 1
+        return "".join(r)
