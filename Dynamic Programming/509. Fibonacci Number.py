b4b607415623f9939735bd6aa43a977eacff7562
hippo2311 <tobaochau05@gmail.com>
2025-01-01 23:19:46 +0700

    new file:   Dynamic Programming/509. Fibonacci Number.py

---
+++ Dynamic Programming/509. Fibonacci Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        a = {0:0, 1:1}
+        def fibsum(x):
+            if x in a:
+                return a[x]
+            else:
+                a[x] = fibsum(x-1) + fibsum(x-2)
+                return a[x]
+    
+        return fibsum(n)
+        
+# Time O(n)
+# Space O(n)
