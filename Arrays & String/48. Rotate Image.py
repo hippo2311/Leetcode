417bd8d47cebe11636632b4c0edb6fd9a2e3ab80
hippo2311 <tobaochau05@gmail.com>
2025-02-09 11:28:10 +0800

    new file:   Arrays & String/48. Rotate Image.py

---
+++ Arrays & String/48. Rotate Image.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        n = len(matrix)
+        
+        # Tranpose
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+# Time: O(N^2)
+# Space: O(1)    
+            
+
+         
+        
